6770fe1134e2ac64b2e14b6656c82dfae7120c7e
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-06 21:18:57 -0300

    add programa de soma com 2 valores

---
+++ exe_02.py
@@ -0,0 +1,9 @@
+# Criar um programa aonde o usuário digite dois valores e apreça a soma 
+
+valor_01 = input("Digite um valor: ")
+
+valor_02 = input("Digite um valor: ")
+
+result_soma = int(valor_01) + int(valor_02)
+
+print("A soma dos valores: " + str(result_soma))
