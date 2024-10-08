739fd3365b3e7560eaf22fae0e1e2399c03ccaaa
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-07 22:11:49 -0300

    criacao do codigo calculo salarial

--- kpi.py
+++ kpi.py
@@ -1,15 +1,15 @@
 # 1) Solicita ao usuário que digite seu nome
-
+nome = input("Digite seu nome: ")
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
-
+salario = float(input("Digite seu salário: "))
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-
+bonus = float(input("Digite o bônus recebido: "))
 # 4) Calcule o valor do bônus final
-
+result = 1000 + salario * bonus
 # 5) Imprima cálculo do KPI para o usuário
-
+print(result)
 # 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
-
+print(f" {nome} seu salário com o bônus fica: {result}")
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
